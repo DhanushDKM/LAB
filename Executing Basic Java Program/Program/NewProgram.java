2e986a765017292dd13ee363f17e3f3a993835bb
DhanushDKM <110026584+DhanushDKM@users.noreply.github.com>
2022-08-08 11:33:51 +0530

    Add files via upload

---
+++ Executing Basic Java Program/Program/NewProgram.java
@@ -0,0 +1,37 @@
+/*
+@Author : Dhanush Kumar M
+Date : 28/07/2022
+Title : New Program
+Question : Executing Basic Java Program
+*/
+
+class NewProgram
+{      
+       //Calling main method
+       public static void main(String[]args)
+       { 
+
+            System.out.println("Start");
+
+            System.out.println("java123");
+            
+			System.out.println(123456);
+
+            System.out.println("10 + 20");
+			
+			System.out.println("10" + "20");
+
+            System.out.println(true); 
+
+            System.out.println(0b0010);
+			
+		    System.out.println(0123456);
+			
+			System.out.println(10+30+"java - "+20+20);
+			
+			System.out.println(10+30+"java -"+(20+40+60));
+			
+			
+		}	
+
+}		
